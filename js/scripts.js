aa50ab484c0af3d1c36970fe28938ce65302803a
Katie Ogden <katie@Katies-MacBook-Pro.local>
2021-03-07 20:14:56 -0700

    finished tutorial from class lectures

--- js/scripts.js
+++ js/scripts.js
@@ -3,52 +3,73 @@
 //'"Creativity takes courage" - Henry Matisse'
 //]
 
-var quote1 = {
-    quote:'"We don\'t make mistakes, just happy little accidents"',
-    author: "Bob Ross",
-    tags: ["painting", "mistakes"],
-    color: "#0A3410",
-    display: function(){
+function Quote(quoteText, author, tags, color) {
+    this.quote = quoteText;
+    this.author = author;
+    this.tags = tags;
+    this.color = color;
+    this.display = function(){
+        
+        var container = $("<div>")
+        this.tags.forEach(function(tag){
+            container.addClass(tag)
+        })
+        
+        container.css("background", this.color)
+        container.addClass("quote")
+        
         var quoteString = "";
-        quoteString += "<div style='background:" + this.color + "'>";
         quoteString += "<p>" + this.quote + "</p>";
         quoteString += "<cite>" + this.author + "</cite>";
-        quoteString += "</div>";
-        $("body").prepend(quoteString)
+        
+        container.html(quoteString)
+        $(".quotes").prepend(container)
     }
 }
 
-var quote2 = {
-    quote:'"Creativity takes courage"',
-    author: "Henry Matisse",
-    tags: ["creativity", "courage"],
-    color: "#ff9966",
-    display: function(){
-        var quoteString = "";
-        quoteString += "<div style='background:" + this.color + "'>";
-        quoteString += "<p>" + this.quote + "</p>";
-        quoteString += "<cite>" + this.author + "</cite>";
-        quoteString += "</div>";
-        $("body").prepend(quoteString)
-    }
-}
+var quotes = [
+    new Quote(
+    '"We don\'t make mistakes, just happy little accidents"', 
+    "Bob Ross", 
+    ["painting", "mistakes"], 
+    "#0A3410"),
 
-quote1.display();
+    new Quote(
+    '"Creativity takes courage"',
+    "Henry Matisse",
+    ["creativity", "painting"],
+    "#ff9966")
 
-quote2.display();
+    ]
 
-console.log(quote1.quote) 
 
-//var quoteString = "";
-//quotes.forEach(function(quote){ 
-//    
-//    quoteString += "<p>" + quote + "</p>"
-//    
-//    console.log(quoteString);
-//    
-//})
-//
-//$("body").html(quoteString)
+// global taglist
+var tagList = []
+quotes.forEach(function(quote){
+    quote.display();
+    quote.tags.forEach(function(tag){
+        // check to see if tag has been added to taglist
+        if(!tagList.includes(tag)) {
+            // if it isn't added, add it
+           tagList.push(tag); 
+            // and also make a button for it
+            $(".buttons").prepend("<button class='filter' id='" + tag + "'>" + tag + "</button>")
+        }
+    })
+}) 
+
+ console.log(tagList)
+
+$(".filter").on("click", function(){
+    var tag = $(this).attr("id");
+    console.log(tag)
+    
+    $("." + tag).fadeIn();
+    $(".quote").not("." + tag).hide();
+    
+    $(".filter").removeClass("active")
+    $(this).addClass("active")
+})
 
 
 
